1aa12a6a20159548ad04dfa10db8a934c8ab09a9
Justin Walker <walkerjt93@gmail.com>
2023-01-26 15:50:00 -0500

    completed palindromes well if i do say so

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+let string = 'Animal loots foliated detail of stool lamina.';
 
+const palindromes = (input)  => {
+  let cleaned = input.replace(/\W/g, '').toLowerCase();
+  let reversed = cleaned.split('').reverse().join('').toString('');
+  return cleaned === reversed;
 };
 
+
+console.log(palindromes(string))
 // Do not edit below this line
 module.exports = palindromes;
